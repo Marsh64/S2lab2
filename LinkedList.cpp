23992ea1105e3e97426608f0a6cf60c879b4386b
Marsh64 <70971960+Marsh64@users.noreply.github.com>
2021-04-06 18:59:54 +0400

    Add files via upload

---
+++ LinkedList.cpp
@@ -0,0 +1,142 @@
+#include "LinkedList.h"
+
+template <class T>
+void LinkedList<T>::Append(T item) {
+    auto *new_cell = new struct element;
+    new_cell->value = item;
+    new_cell->next_element = nullptr;
+
+    if (head_element == nullptr){ //случай, когда список пустой
+        head_element = new_cell;
+        end_element = new_cell;
+        len = 1;
+        return;
+    }
+
+    end_element->next_element = new_cell;
+    end_element = new_cell;
+    len++;
+}
+
+template <class T>
+LinkedList<T>::LinkedList() {
+    len = 0;
+    head_element = nullptr;
+    end_element = nullptr;
+}
+
+template <class T>
+LinkedList<T>::LinkedList(T *items, int count) {
+    len = 0;
+    head_element = nullptr;
+    end_element = nullptr;
+
+    for (int i = 0; i < count; i++){
+        Append(items[i]);
+    }
+}
+
+template <class T>
+LinkedList<T>::LinkedList(const LinkedList<T> &list) {
+    len = 0;
+    head_element = nullptr;
+    end_element = nullptr;
+    struct element *cell = list.head_element;
+    for (int i = 0; i < list.len; i++, cell = cell->next_element){
+        Append(cell->value);
+    }
+}
+
+template <class T>
+T LinkedList<T>::GetFirst() {
+    if (head_element == nullptr){throw IndexOutOfRange();}
+
+    return head_element->value;
+}
+
+template <class T>
+T LinkedList<T>::GetLast() {
+    if (end_element == nullptr){throw IndexOutOfRange();}
+
+    return end_element->value;
+}
+
+template <class T>
+T LinkedList<T>::Get(int index) {
+    if (index < 0 || index >= len){throw IndexOutOfRange();}
+
+    struct element *cell = head_element;
+    for (int i = 0; i < index; i++, cell = cell->next_element);
+
+    return cell->value;
+}
+
+template <class T>
+LinkedList<T> LinkedList<T>::GetSubList(int startIndex, int endIndex) {
+    if (startIndex >= len || endIndex >= len || startIndex < 0 || endIndex < 0){
+        throw IndexOutOfRange();
+    }
+
+    LinkedList<T> new_list = LinkedList<T>();
+    auto *cell = new struct element;
+    cell = head_element;
+    for (int i = 0; i < startIndex; i++, cell = cell->next_element);
+    for (int i = startIndex; i < endIndex; i++, cell = cell->next_element){
+        new_list.Append(cell->value);
+    }
+
+    return new_list;
+}
+
+template <class T>
+int LinkedList<T>::GetLength() {
+    return len;
+}
+
+template <class T>
+void LinkedList<T>::Prepend(T item) {
+    auto *new_cell = new struct element;
+    new_cell->value = item;
+
+    if (head_element == nullptr){
+        end_element = new_cell;
+    }
+
+    new_cell->next_element = head_element;
+    head_element = new_cell;
+    len++;
+}
+
+template <class T>
+void LinkedList<T>::InsertAt(T item, int index) {
+    if (index >= len || index < 0){throw IndexOutOfRange();}
+
+    if (index == 0){Append(item); return;}
+    else if (index == len - 1){Prepend(item); return;}
+    else{
+        auto *new_cell = new struct element;
+        new_cell->value = item;
+        struct element *previous_cell = head_element;
+        for (int i = 0; i < index - 1; i++, previous_cell = previous_cell->next_element);
+        new_cell->next_element = previous_cell->next_element;
+        previous_cell->next_element = new_cell;
+        len++;
+    }
+}
+
+template<class T>
+LinkedList<T> * LinkedList<T>::Concat(LinkedList<T> *list) {
+    LinkedList<T> new_list = LinkedList<T>();
+
+    struct element *new_cell = head_element;
+    for (int i = 0; i < len; i++, new_cell = new_cell->next_element){
+        new_list.Append(new_cell->value);
+    }
+
+    new_cell = list->head_element;
+    for (int i = 0; i < list->len; i++, new_cell = new_cell->next_element){
+        new_list.Append(new_cell->value);
+    }
+
+    return &new_cell;
+}
---
+++ LinkedList.h
@@ -0,0 +1,33 @@
+
+template <class T>
+class LinkedList {
+private:
+    struct element{
+        T value;
+        struct element* next_element;
+    };
+    int len;
+    struct element* head_element = nullptr;
+    struct element* end_element = nullptr;
+
+public:
+    class IndexOutOfRange{};
+
+    //Создание объекта
+    LinkedList (T* items, int count); //Копирует элементы из переданного списка
+    LinkedList (); //Создает пустой список
+    LinkedList (const LinkedList <T> & list); //Копирующий конструктор
+
+    //декомпозиция
+    T GetFirst();//Возвращает первый элемент
+    T GetLast();//Возвращает последний элемент
+    T Get(int index);//Возвращает элемент по индексу
+    LinkedList<T> GetSubList(int startIndex, int endIndex);//Возваращет список элементов в исходных пределах
+    int GetLength();//Возвращает длину
+
+    //Операции
+    void Append(T item); //Добавление элемента в конец списка
+    void Prepend(T item); //Добавление элемента в начало списка
+    void InsertAt(T item, int index); //Вставка элемента в заданную позицию
+    LinkedList<T>* Concat(LinkedList<T> *list); //Сцепляет два списка
+};
