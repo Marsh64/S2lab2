1b2efd4c26947901cb2af4272673354fe13c6029
Marsh64 <70971960+Marsh64@users.noreply.github.com>
2021-04-08 02:11:57 +0400

    Add files via upload

---
+++ LinkedListSequence.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adm on 08.04.2021.
+//
+
+#include "LinkedListSequence.h"
---
+++ LinkedListSequence.h
@@ -0,0 +1,56 @@
+#include "Sequence.h"
+#include "LinkedList.h"
+
+template <class T>
+class LinkedListSequence : Sequence<T>{
+private:
+    LinkedList<T> listSequence;
+public:
+    class IndexOutOfRange : LinkedList<T>::IndexOutOfRange{};
+
+    //Создание объекта
+    LinkedListSequence (T* items, int count){
+        listSequence = LinkedList<T>(items, count);
+    }//Копирует элемнеты из переданного массива
+    LinkedListSequence (){
+        listSequence = LinkedList<T>();
+    }//Создает пустую последовательность
+    explicit LinkedListSequence (const LinkedList <T> & list){
+        listSequence = LinkedList<T>(list);
+    }//Копирующий конструктор
+
+    //Декомпозиция
+    T GetFirst(){
+        return listSequence.GetFirst();
+    }//Возвращает первый элемент
+    T GetLast(){
+        return listSequence.GetLast();
+    }//Возвращает последний элемент
+    T Get(int index){
+        return listSequence.Get(index);
+    }//Возвращает элемент по индексу
+    LinkedListSequence<T>* GetSubsequence(int startIndex, int endIndex){
+        auto *new_LinkedList = new LinkedList<T>;
+        new_LinkedList = listSequence.GetSubList(startIndex, endIndex);
+        return new_LinkedList;
+    }//Получить список из всех элементов, начиная с startIndex и заканчивая endIndex
+    int GetLength(){
+        return listSequence.GetLength();
+    }
+
+    //Операции
+    void Append(T item){
+        listSequence.Append(item);
+    }//Добавляет элемент в конец списка
+    void Prepend(T item){
+        listSequence.Prepend(item);
+    }//Добавляет элемент в начало списка
+    void InsertAt(T item, int index){
+        listSequence.InsertAt(item, index);
+    }//Вставляет элемент в заданную позицию
+    LinkedListSequence<T>* Concat(LinkedListSequence<T> *list){
+        auto *new_LinkedList = new LinkedListSequence<T>;
+        new_LinkedList->listSequence = listSequence.Concat(list->listSequence);
+        return new_LinkedList;
+    }//Сцепляет два списка
+};
